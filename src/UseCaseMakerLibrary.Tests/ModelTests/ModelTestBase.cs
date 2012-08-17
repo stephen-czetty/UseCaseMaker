721517938c2b0840cca9d36ce8e21e9ff63b4534
Stephen M. Czetty <czettys@advisory.com>
2012-08-17 18:26:50 -0500

    Completed coverage of Model (excluding some constructors)

--- src/UseCaseMakerLibrary.Tests/ModelTests/ModelTestBase.cs
+++ src/UseCaseMakerLibrary.Tests/ModelTests/ModelTestBase.cs
@@ -1,4 +1,6 @@
-﻿using Machine.Specifications;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
 using UMMO.TestingUtils;
 
 namespace UseCaseMakerLibrary.Tests.ModelTests
@@ -22,4 +24,20 @@ namespace UseCaseMakerLibrary.Tests.ModelTests
         protected static Model Model;
         protected static string ModelName;
     }
+
+    // Fill in data for the Model
+    // Don't need this for coverage of Model, but we'll need something like it later for coverage of Package
+    //int id = 1;
+    //_startTag = A.Random.String.Resembling.A.Noun;
+    //_endTag = A.Random.String.Resembling.A.Noun;
+    //_name = A.Random.String.Resembling.A.Verb;
+    //string fullName = _startTag + _name + _endTag;
+    //Model.AddRequrement();
+    //var useCase = new UseCase {ID = id++, Prose = fullName + A.Random.String};
+    //int idx = useCase.AddOpenIssue();
+    //((OpenIssue) useCase.OpenIssues[idx]).Description = fullName +
+    //                                                    A.Random.String;
+    //idx = useCase.AddStep(null, Step.StepType.Default, A.Random.String, useCase, DependencyItem.ReferenceType.Client);
+    //((Step)useCase.Steps[idx]).Description = fullName + A.Random.String;
+    //Model.AddUseCase(useCase);
 }
