883157ecc69d29e7644dcbec0740206a61cb0766
Stephen M. Czetty <czettys@advisory.com>
2012-11-19 09:35:29 -0600

    Split Ioc Serializer tests into seperate files

---
+++ src/UseCaseMaker.Ioc.Tests/SerializerSelectorTestsBase.cs
@@ -0,0 +1,19 @@
+using System.IO;
+
+namespace UseCaseMaker.Ioc.Tests
+{
+    public abstract class SerializerSelectorTestsBase
+    {
+        protected static string TempFileName;
+
+        protected static void CreateTempFile(string version)
+        {
+            TempFileName = Path.GetTempFileName();
+            using (var textWriter = new StreamWriter(TempFileName))
+            {
+                textWriter.WriteLine(@"<?xml version=""1.0"" encoding=""utf-8"" ?>");
+                textWriter.WriteLine(@"<rootNode Version=""" + version + @"""/>");
+            }
+        }
+    }
+}
